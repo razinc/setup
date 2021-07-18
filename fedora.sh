e7a84672b109db384750f201606e4f5cac7ceb8c
razinc <l_noir@yahoo.com>
2021-07-18 15:25:26 +0800

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -4,3 +4,7 @@ sudo dnf install i3-gaps
 
 # install neofetch
 sudo dnf install neofetch
+
+# install steam
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install steam
