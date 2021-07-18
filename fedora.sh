07e76dddbd958fc64a5efaa4406007f459a5ca52
razinc <l_noir@yahoo.com>
2021-07-18 16:41:29 +0800

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -5,6 +5,13 @@ sudo dnf install i3-gaps
 # install neofetch
 sudo dnf install neofetch
 
+# install nvidia driver
+# disable secure boot
+sudo dnf install dnf-plugins-core -y
+sudo dnf copr enable t0xic0der/nvidia-auto-installer-for-fedora -y
+sudo dnf install nvautoinstall -y
+sudo nvautoinstall --rpmadd
+
 # install steam
 sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install steam
