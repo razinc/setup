6dd3c47affc3d940a5babca691ba0ab3338a2ade
razinc <l_noir@yahoo.com>
2021-07-24 00:12:06 +0800

    Update fedora.sh

--- install/fedora.sh
+++ install/fedora.sh
@@ -1,32 +1,28 @@
-# install i3-gaps
-sudo dnf copr enable fuhrmann/i3-gaps
-sudo dnf install i3-gaps
-sudo dnf install feh
+sudo dnf copr enable fuhrmann/i3-gaps -y
+sudo dnf install i3-gaps -y
 
-# install neofetch
-sudo dnf install neofetch
-
-# install nvidia driver
 # disable secure boot
 sudo dnf install dnf-plugins-core -y
 sudo dnf copr enable t0xic0der/nvidia-auto-installer-for-fedora -y
 sudo dnf install nvautoinstall -y
-sudo nvautoinstall --rpmadd
+sudo nvautoinstall --rpmadd -y
 
-# install steam
-sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install steam
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install steam -y
 
-# install pavucontrol
-sudo dnf install pavucontrol
-
-# install neovim
-sudo dnf install -y neovim python3-neovim
-
-# install alacritty
-sudo dnf install alacritty
-
-# install zsh
-sudo dnf install zsh
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install vlc -y
+    
+sudo dnf install zsh -y
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo dnf install feh -y
+sudo dnf install neofetch -y
+sudo dnf install i3status -y
+sudo dnf install dmenu -y
+sudo dnf install pavucontrol -y
+sudo dnf install neovim python3-neovim -y
+sudo dnf install alacritty -y
+
