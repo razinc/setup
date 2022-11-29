b3c4fe84b0e11433f4d1d830fb528a495660f90d
razinc <l_noir@yahoo.com>
2022-11-29 15:19:02 +0800

    remove unused package

--- install/fedora.sh
+++ install/fedora.sh
@@ -24,36 +24,15 @@ sudo dnf install \
         feh \
         -y | tee install.log
 
-# # i3-gaps
-# sudo dnf copr enable fuhrmann/i3-gaps -y | tee -a install.log
-# sudo dnf install \
-#         i3-gaps \
-#         polybar \
-#         rofi \
-#         feh \
-#         -y | tee -a install.log
-
 # font
 wget -P /tmp https://github.com/FortAwesome/Font-Awesome/releases/download/6.2.0/fontawesome-free-6.2.0-desktop.zip
 unzip /tmp/fontawesome-free-6.2.0-desktop.zip -d /tmp
 sudo cp -r  /tmp/fontawesome-free-6.2.0-desktop/otfs /usr/local/share/fonts/FontAwesome6
 fc-cache -f -v
 
-# brave
-sudo dnf install dnf-plugins-core
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser -y
-
 # steam
 sudo dnf config-manager --set-enabled rpmfusion-nonfree-steam  | tee -a install.log
 sudo dnf install steam -y  | tee -a install.log
-
-# # nvidia driver (disable secure boot)
-# sudo dnf config-manager --set-enabled rpmfusion-nonfree-nvidia-driver  | tee -a install.log
-# sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda -y  | tee -a install.log
-# # check if nvidia driver is installed
-# #nvidia-smi
 
 # zshell
 sudo dnf install zsh -y  | tee -a install.log
