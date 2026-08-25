063eaf4c6701a2685a661aa0ff0c99996edddf60
razinc <l_noir@yahoo.com>
2026-08-25 19:20:12 +0800

    remove comments and print commands

--- dotfiles/export.sh
+++ dotfiles/export.sh
@@ -1,31 +1,17 @@
 #!/usr/bin/bash
 
-# set source
 src=`pwd`
-printf "Source directory: $src\n\n"
 
-# vim
-printf "exporting vi"
 rm -rf ~/.config/nvim
 ln -svf $src/.config/nvim ~/.config/nvim
 
-# profile
-printf "exporting profile"
 ln -svf $src/.profile ~/.profile
 
-# oh-my-zsh
-
-# zshrc
-printf "exporting zsh"
 ln -svf $src/.zshrc ~/.zshrc
 
-# mangohud
-printf "exporting mangohud"
 rm -rf ~/.config/MangoHud
 ln -svf $src/.config/MangoHud ~/.config/MangoHud
 
-# openrgb
-printf "exporting openrgb"
 mkdir -p ~/.config/OpenRGB
 ln -svf $src/.config/OpenRGB/eva.orp ~/.config/OpenRGB
 ln -svf $src/.config/OpenRGB/off.orp ~/.config/OpenRGB
